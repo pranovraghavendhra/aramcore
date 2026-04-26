a3eca270ea24c6ef67303e43db024994921542ba
Pranov <pranovraghavendhra@gmail.com>
2026-04-26 19:04:48 +0000

    kernel: protected mode + C entry point working

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -1,5 +1,4 @@
 BITS 16
-ORG 0x7C00
 
 start:
     cli
@@ -9,20 +8,54 @@ start:
     mov ss, ax
     mov sp, 0x7C00
 
-    mov si, msg
-.print:
-    lodsb
-    or al, al
-    jz halt
-    mov ah, 0x0E
-    int 0x10
-    jmp .print
+    ; switch to 32bit protected mode
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp CODE_SEG:protected_mode
 
-halt:
+BITS 32
+protected_mode:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ebp, 0x90000
+    mov esp, ebp
+
+    ; hand control to kernel_main in C
+    extern kernel_main
+    call kernel_main
     hlt
-    jmp halt
 
-msg db "aramcore booting...", 0x0D, 0x0A, 0
+; GDT - tells cpu about memory segments
+gdt_start:
+    dq 0x0
+gdt_code:
+    dw 0xFFFF
+    dw 0x0
+    db 0x0
+    db 10011010b
+    db 11001111b
+    db 0x0
+gdt_data:
+    dw 0xFFFF
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 TIMES 510 - ($ - $$) db 0
 DW 0xAA55
---
+++ kernel/kernel.c
@@ -0,0 +1,6 @@
+/* aramcore - kernel entry point */
+void kernel_main(void)
+{
+    /* nothing yet, just halts cpu */
+    while(1);
+}
